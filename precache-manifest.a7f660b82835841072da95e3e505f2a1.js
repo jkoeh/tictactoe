268adaed92743e9626e46f6b04cfebd820fc974f
jkoeh <johnkoeh@gmail.com>
2021-03-15 09:33:20 -0700

    Updates

---
+++ precache-manifest.a7f660b82835841072da95e3e505f2a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d5d37919d131ad1ceef7c3dd813e77",
+    "url": "/jkoeh/tictactoe/index.html"
+  },
+  {
+    "revision": "f3104f1eaa0f0d3c64cc",
+    "url": "/jkoeh/tictactoe/static/css/main.65cef629.chunk.css"
+  },
+  {
+    "revision": "c7a72b7db28438d76fb1",
+    "url": "/jkoeh/tictactoe/static/js/2.3676add4.chunk.js"
+  },
+  {
+    "revision": "f3104f1eaa0f0d3c64cc",
+    "url": "/jkoeh/tictactoe/static/js/main.d56e6bcf.chunk.js"
+  },
+  {
+    "revision": "0ae7557b474b09e5310a",
+    "url": "/jkoeh/tictactoe/static/js/runtime-main.6d455457.js"
+  }
+]);
